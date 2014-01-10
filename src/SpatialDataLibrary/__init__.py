23fbb28b4b12f0e4420495d606b32f8be517eff7
Peter Goulborn <petergoulborn@astuntechnology.com>
2014-01-10 11:37:42 +0000

    Added Get Geometry Column, use 'extent'
    Get Geometry Column will get the geometry column for a given table.
    
    Table Bounds Should Equal now Table Extent Should Equal, this is to
    allow for clearer future use of the word 'boundary' and because most GIS
    software that we use prefers this term.
    
    Also updated extent checking to return one error for any number of
    non-matching coords.

--- src/SpatialDataLibrary/__init__.py
+++ src/SpatialDataLibrary/__init__.py
@@ -22,39 +22,61 @@ __version__ = '0.1'
 class SpatialDataLibrary(DatabaseLibrary):
     """
     """
-    def table_bounds_should_equal(self, tablename, bbox, 
+
+    def get_geometry_column(self, tablename):
+        """
+        Gets the geometry column name from "geometry_columns", example:
+        | ${col} | Get Geometry Column | my_table |
+        | Should Be Equal | wkb_geometry | ${col} |
+
+        """
+        self.table_must_exist(tablename)
+        statement = '''
+        SELECT f_geometry_column
+        FROM geometry_columns
+        WHERE f_table_name = '{}';'''.format(tablename)
+        return self._get_single_result(statement)
+
+
+    def table_extent_should_equal(self, tablename, extent,
                                   geometry_column='wkb_geometry'):
         """
-        
-        Checks that the bounding box of a given table matches that specified
-        
+
+        Checks that the bounding box of a given table matches that specified.
+
         Bounding box must be specified as a comma separated string of the form
-        MinX,MinY,MaxX,MaxY, e.g.:
-            100000,300000,200000,400000
+        MinX,MinY,MaxX,MaxY, for example:
+
+        | Table Extent Should Equal | my_table | 100000,300000,200000,400000 |
+
+        Units are those used by the projection defined for the table (metres
+        for the above example, where the table is in EPSG:27700).
+
         """
-        statement = 'SELECT ST_Extent("{}") FROM {};'.format(geometry_column, 
+        statement = 'SELECT ST_Extent("{}") FROM {};'.format(geometry_column,
                                                           tablename)
-        bounds = bbox.split(',')
-        table_bbox = self.query(statement)[0][0]
-        table_bbox = table_bbox[4:-1]  # Remove 'BOX(' at start, ')' from end
-        table_bbox = ','.join(table_bbox.split(' '))
-        table_bounds = table_bbox.split(',')
+        bounds = extent.split(',')
+        if len(bounds) != 4:
+            msg = 'Wrong number of points in extent: {}'.format(len(bounds))
+            raise RuntimeError(msg)
+        table_extent = self._get_single_result(statement)
+        table_extent = table_extent[4:-1]  # Chop 'BOX(' and  ')'
+        table_extent = ','.join(table_extent.split(' '))
+        table_bounds = table_extent.split(',')
         errors = []
         for i in range(4):
             try:
                 coord = 'X' if i % 2 else 'Y'
                 adj = 'Min' if i < 2 else 'Max'
                 error_msg = '{}{} values don\'t match'.format(adj, coord)
-                builtin.should_be_equal_as_numbers(bounds[i], 
-                                                   table_bounds[i], 
+                builtin.should_be_equal_as_numbers(bounds[i],
+                                                   table_bounds[i],
                                                    msg=error_msg, values=True)
             except AssertionError as ae:
                errors.append(str(ae))
-        
+
         if len(errors):
             raise AssertionError('\n'.join(errors))
-              
-        
-        
-    
+
+
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
